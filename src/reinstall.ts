fc141573cbec52033716a6e4bf229250de390a5c
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2023-04-06 20:55:01 +0200

    feat: update action reinstall

--- src/reinstall.ts
+++ src/reinstall.ts
@@ -1,21 +1,12 @@
-import {program} from 'commander';
-import {Console} from './services';
-import {commanderOptions} from './utils';
+import {Cli, Console} from './services';
+import {install} from './actions/install';
+import {uninstall} from './actions/uninstall';
 import {config} from './config';
 
-const {projectName} = config;
-
 /**
- * Entry
+ * Reinstallation process
  */
-program
-	.description(`Reinstalls ${projectName}. Calls commands "uninstall" and "install" in sequence. Saves personal data.`)
-	.addOption(commanderOptions.include)
-	.addOption(commanderOptions.exclude)
-	.addOption(commanderOptions.reinstall)
-	.parse();
-
-await import('./uninstall');
-await import('./install');
-
-Console.success(`Project ${projectName} was successfully reinstalled.`);
+const args = Cli.handleArgs(Cli.Actions.Reinstall);
+await uninstall(args);
+await install(args);
+Console.success(`Project ${config.projectName} was successfully reinstalled.`);
