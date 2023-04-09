a8c7e60274c8271c9699be807eeb2082957cd472
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2023-04-09 21:37:27 +0200

    fix: hide double console outputs in watch mode

--- src/actions/start/nodemonMiddleware.ts
+++ src/actions/start/nodemonMiddleware.ts
@@ -2,5 +2,5 @@ import {Cli} from '~/services';
 import {bootProject} from './bootProject';
 
 // Arguments are parsed a second time if called by nodemon
-const args = Cli.handleArgs(Cli.Actions.Start);
+const args = Cli.mute.handleArgs(Cli.Actions.Start);
 await bootProject(args);
